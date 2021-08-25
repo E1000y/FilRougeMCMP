eb04ebdc1da7ace5c5bb973d39dd01ad1615f865
Emilie Paniagua <emilie.paniagua@gmail.com>
2021-08-25 18:35:11 +0200

    Added droits

---
+++ Requetes/droits.sql
@@ -0,0 +1,47 @@
+    grant select on activite to cda5pani_proj_orga;
+    grant update on activite to cda5pani_proj_orga;
+    grant delete on activite to cda5pani_proj_orga;
+    grant insert on activite to cda5pani_proj_orga;
+
+    grant select on cotiser to cda5pani_proj_orga;
+    grant update on cotiser to cda5pani_proj_orga;
+    grant delete on cotiser to cda5pani_proj_orga;
+    grant insert on cotiser to cda5pani_proj_orga;
+
+    grant select on membre to cda5pani_proj_orga;
+    grant update on membre to cda5pani_proj_orga;
+    grant delete on membre to cda5pani_proj_orga;
+    grant insert on membre to cda5pani_proj_orga;
+
+
+    grant select on participer to cda5pani_proj_orga;
+    grant update on participer to cda5pani_proj_orga;
+    grant delete on participer to cda5pani_proj_orga;
+    grant insert on participer to cda5pani_proj_orga;
+
+
+    grant select on photo to cda5pani_proj_orga;
+    grant update on photo to cda5pani_proj_orga;
+    grant delete on photo to cda5pani_proj_orga;
+    grant insert on photo to cda5pani_proj_orga;
+
+    grant select on publication to cda5pani_proj_orga;
+    grant update on publication to cda5pani_proj_orga;
+    grant delete on publication to cda5pani_proj_orga;
+    grant insert on publication to cda5pani_proj_orga;
+
+    grant select on saison to cda5pani_proj_orga;
+    grant update on saison to cda5pani_proj_orga;
+    grant delete on saison to cda5pani_proj_orga;
+    grant insert on saison to cda5pani_proj_orga;
+
+
+
+
+grant select on activite to cda5pani_proj_adh;
+grant select on cotiser to cda5pani_proj_adh;
+grant select on membre to cda5pani_proj_adh;
+grant select on participer to cda5pani_proj_adh;
+grant select on photo to cda5pani_proj_adh;
+grant select on publication to cda5pani_proj_adh;
+grant select on saison to cda5pani_proj_adh;
